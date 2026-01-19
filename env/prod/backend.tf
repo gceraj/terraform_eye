648060565c0928a380832731cc0cdccc7f38c41a
PAKKIARAJ T <GCERAJ@GMAIL.COM>
2026-01-19 17:42:45 -0500

    4 added s3 bucket

---
+++ env/prod/backend.tf
@@ -0,0 +1,10 @@
+terraform {
+  backend "s3" {
+    bucket         = "gceraj-terraform-state"   # your bucket
+    key            = "prod/terraform.tfstate"   # path inside bucket
+    region         = "us-east-1"                # or your region
+    use_lockfile   = true          # lock table name
+    dynamodb_table = "gceraj-terraform-locks"
+    encrypt        = true
+  }
+}
